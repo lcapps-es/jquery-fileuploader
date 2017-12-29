44ac8ea1f2075aaff85160a797112e26ed7467bf
lmmartinb <lm.martinb@gmail.com>
2017-12-29 01:57:32 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # jquery-fileuploader
 jQuery FileUploader Plugin.
+
+## Usage
+You only need to add the following line to initialize the plugin.
+
+$(div).fileUploader();
+
+## How It Works
+This is a simple plugin:
+
+1. Creates a div where the image will be previewed.
+2. Initialize fileUploader plugin: $(div).fileUploader();
+3. A canvas fill 100% div's area.
+4. A hidden input type file will be added.
+5. Append onclick event listener in canvas.
+6. Onchange event for input file that draw an image into canvas.
+7. Rotate canvas reading EXIF data.
+
+
+## License
+```
+MIT License
+
+Copyright (c) 2017 Luis Miguel Martín Bardanca
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+```
+
+## To-Do List
+* FileUploader Options.
+* Modal handler.
+* Multi image.
+* Video support.
+* Cropper.
+* Rotate buttons.
+
+(If you have any suggestion please feel free to contact me)
+
+## History of Changes
+
+### v1.0.0
+Initial version
---
+++ js/fileUploader.js
@@ -0,0 +1,267 @@
+/**
+ * JavaScript FileUploader Class.
+ * 
+ * The way to use this plugin is as follows:
+ * 	1. Creates a div where the image will be previewed
+ *	2. Initialize fileUploader plugin: $(div).fileUploader();
+ * 
+ * @example $(div).fileUploader();
+ * @author Luis Miguel Martín
+ */
+
+'use strict';
+
+/**
+ * jQuery fileUploader extension.
+ */
+$.fn.extend({
+	fileUploader: function( options ){
+		// fileUploader Options.
+		var options = ( options === undefined ) ? {} : options;
+		FileUploader.Options.processOptions( options );
+
+		// for each element, load fileUploader elements.
+		this.each(function(){
+			FileUploader.load( this );
+		});
+	}
+
+});
+
+/**
+ * FileUploader Methods.
+ */
+var FileUploader = {
+	/**
+	 * Attributes.
+	 */
+	fileType: "image/*", // this must be null
+
+
+	/**
+	 * Methods.
+	 */
+
+	/**
+	 * Main Method. Add elements into div.
+	 */
+	load: function( el ) {
+		FileUploader.Makers.appendElements( el );
+		FileUploader.Makers.addClickEvents( el );
+	},
+
+	/**
+	 * Options Methods.
+	 */
+	Options: {
+
+		/**
+		 * This method validate input options.
+		 * 
+		 * Dummy...
+		 */
+		validateOptions: function( options ) {
+			// nothing yet...
+		},
+
+		/**
+		 * This method process options.
+		 * 
+		 * Dummy...
+		 */
+		processOptions: function( options ) {
+			FileUploader.Options.validateOptions( options );
+
+			// nothing yet....
+		}
+
+	},
+
+	/**
+	 * Makers Methods.
+	 */
+	Makers: {
+
+		/**
+		 * Appends needed elements into div.
+		 */
+		appendElements: function( el ) {
+			// Gets div info.
+			var width = $(el).width();
+			var height = $(el).height();
+			var id = $(el).attr('id');
+	
+			// Append canvas.
+			var canvas = $('<canvas id="canvas_'+id+'" width="'+width+'" height="'+height+'"></canvas>');
+			$(el).append( canvas );
+	
+			// Only accept's custom fileType.
+			var fileType = ( FileUploader.fileType !== null ) ? ' accept="'+FileUploader.fileType+'" '  : "";
+	
+			// Append input file.
+			var inputFile = $('<input onchange="FileUploader.Makers.change(this)" id="file_'+id+'" type="file" style="display:none" '+fileType+'/>');
+			$(el).append( inputFile );
+	
+		},
+
+		/**
+		 * Creates canvas click event.
+		 */
+		addClickEvents: function( el ) {
+			var id = $(el).attr('id');
+	
+			$( '#canvas_'+id ).click(function(){
+				$( '#file_'+id ).trigger('click');
+			});
+		},
+
+		/**
+		 * Onchange's file method.
+		 */
+		change: function( el ) {
+			var id = $(el).attr('id');
+
+			// Get file
+			var f = FileUploader.Files.getFile( id );
+
+			// Get File Orientation.
+			FileUploader.Files.getOrientation( f, function( orientation ) {
+
+				var file = new FileReader();
+				file.readAsDataURL( f );
+				file.onload = function( ev ) {
+					// changeCanvas to show image.
+					FileUploader.Files.changeCanvas( ev, el, orientation );
+				}
+
+			});
+		}
+
+	},
+	
+	/**
+	 * Files Methods.
+	 */
+	Files: {
+
+		/**
+		 * Get file loaded.
+		 */
+		getFile: function( inputId ){
+			// Includes galleryCamera plugin compatibility.
+			if( navigator !== undefined && navigator.galleryCamera !== undefined ) {
+				return navigator.galleryCamera.getFile( inputId );
+			}else{
+				return document.getElementById( inputId ).files[0];
+			}
+		},
+
+		/**
+		 * Change canvas drawing image.
+		 */
+		changeCanvas: function( ev, el, orientation ) {
+			var canvasId = $(el).prev().attr('id');
+
+			// Revert every image transformation.
+			$('#'+canvasId).css({
+				'transform': '',
+			});
+
+			// load image.
+			var image = new Image();
+			image.src = ev.target.result;
+			image.onload = function() {
+				// get canvas size.
+				var canvas = document.getElementById( canvasId );
+				var fullHeight = canvas.height;
+				var fullWidth = canvas.width;
+
+				// gets ratio between width & height.
+				var diff = image.width / image.height;
+	
+				if( image.height > image.width ){
+					// height will be max canvas size, width variable.
+					var height = fullHeight;
+					var width = height * diff;
+				}else{
+					// width will be max canvas size, height variable.
+					var width = fullWidth;
+					var height = width / diff;
+				}
+
+				var x = ((canvas.width - width) / 2);
+				var y = ((canvas.height - height) / 2);
+	
+				// print image into canvas.
+				var ctx = canvas.getContext("2d");
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.drawImage(image, x, y, width, height);
+				
+				// rotate image if needed.
+				FileUploader.Files.rotate( canvasId, orientation );
+			}
+		},
+
+		/**
+		 * Get orientation from Exif.
+		 */
+		getOrientation: function ( f, callback ) {
+
+			var file = new FileReader();
+			file.readAsArrayBuffer( f );
+			file.onload = function( e ) {
+				var view = new DataView(e.target.result);
+				if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+				var length = view.byteLength, offset = 2;
+				while (offset < length) {
+					var marker = view.getUint16(offset, false);
+					offset += 2;
+					if (marker == 0xFFE1) {
+						if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+						var little = view.getUint16(offset += 6, false) == 0x4949;
+						offset += view.getUint32(offset + 4, little);
+						var tags = view.getUint16(offset, little);
+						offset += 2;
+						for (var i = 0; i < tags; i++)
+							if (view.getUint16(offset + (i * 12), little) == 0x0112)
+								return callback(view.getUint16(offset + (i * 12) + 8, little));
+					}
+					else if ((marker & 0xFF00) != 0xFF00) break;
+					else offset += view.getUint16(offset, false);
+				}
+				return callback(-1);
+			}
+		},
+
+		/**
+		 * Rotate images if needed.
+		 */
+		rotate: function( canvasId, orientation ){
+			if( orientation > 0 && orientation < 9 ){
+				// flip if needed
+				var flip = "";
+				if( orientation == 2){
+					$('#'+canvasId).css({
+						'transform': 'scaleX(-1)',
+					});
+				}else if( orientation == 5 || orientation == 7 || orientation == 4 ){
+					flip = ' scaleX(-1)';
+				}
+				// rotate.
+				if( orientation == 5 || orientation == 6 ){
+					$('#'+canvasId).css({
+						'transform': 'rotate(90deg)'+flip,
+					});
+				}else if( orientation == 3 || orientation == 4 ){
+					$('#'+canvasId).css({
+						'transform': 'rotate(180deg)'+flip,
+					});
+				}else if( orientation == 7 || orientation == 8 ){
+					$('#'+canvasId).css({
+						'transform': 'rotate(-90deg)'+flip,
+					});
+				}
+			}
+		}
+	}
+}
